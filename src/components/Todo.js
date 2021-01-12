ab488923cfc47e71ad81d859be56fdf9a3dd57f2
Sanket Patil <sanket.9006@gmail.com>
2021-01-12 21:49:04 +0530

    Todo List (Delete + Tick)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -13,7 +13,7 @@ const ToDo = ({text, todo, todos, setTodos}) => {
                 if(item.id === todo.id)
                 {
                     return{
-                        ...item , completed: !item.completed 
+                        ...item , completed: !item.completed    
                     };
                 }
                 return item;
@@ -23,12 +23,12 @@ const ToDo = ({text, todo, todos, setTodos}) => {
 
     return(
         <div className="todo">
-            <li className="todo-item">{text}</li>
-
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
+            
             <button onClick={completeHandler} className="complete-btn"> <i className="fas fa-check"> </i></button>
             <button onClick={deleteHandler} className="trash-btn"> <i className="fas fa-trash"> </i></button>
         </div>
     )
 }
 
-export default ToDo;
+export default ToDo;    
